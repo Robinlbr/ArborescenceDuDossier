c977332bc192f05e102d60c2a14b324f38b26bea
AxelLy <46557739+AxelLy@users.noreply.github.com>
2019-02-18 11:04:37 +0100

    Add files via upload

---
+++ gfserveur.js
@@ -0,0 +1,25 @@
+//gfserveur.js
+var http = require("http");
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var controleur = require("./gfcontroleur");
+var laconfig = require("./config.js");
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", null);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  next();
+});
+app.get('/', (req, res) => {
+  var result = controleur.Controller.Files();
+  res.json(result);
+});
+app.listen(laconfig.port, function () {
+  console.log('Example app listening on port '+laconfig.port)
+})
+http.createServer(function(request, response) {
+  response.end('ok');
+}).listen(8000);
+
