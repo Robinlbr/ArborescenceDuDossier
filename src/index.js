0652286100d6844a7057976e5510420aa4171dd0
Dim31 <46557331+Dim31@users.noreply.github.com>
2019-02-18 12:19:11 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import m from "mithril"
-import List from "C:/Users/Dimi/Documents/GitHub/GestionFichier/myapp/src/list.js"
-import Chart from "C:/Users/Dimi/Documents/GitHub/GestionFichier/myapp/src/chart.js"
+import List from "./list.js"
+import Chart from "./chart.js"
 var root = document.body
 List.loadList()
 Chart.loadList()
